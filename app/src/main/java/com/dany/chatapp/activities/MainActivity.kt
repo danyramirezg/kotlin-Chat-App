409f7867751129404a85f78c6152ad4aae5a50c8
danyramirezg <danyramirezg@gmail.com>
2021-04-07 11:14:21 -0500

    Chat app

---
+++ app/src/main/java/com/dany/chatapp/activities/MainActivity.kt
@@ -0,0 +1,88 @@
+package com.dany.chatapp.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.*
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import com.dany.chatapp.R
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_main.view.*
+
+class MainActivity : AppCompatActivity() {
+
+    private var mySectionPagerAdapter: SectionPagerAdapter? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        setSupportActionBar(toolbar)
+        mySectionPagerAdapter = SectionPagerAdapter(supportFragmentManager)
+
+        //container.adapter = mySectionPagerAdapter
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId
+        if(id == R.id.action_settings){
+            return true
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+    inner class SectionPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
+        override fun getItem(position: Int): Fragment {
+            return PlaceHolderFragment.newIntent(position + 1)
+        }
+
+        override fun getCount(): Int {
+            return 3
+        }
+
+    }
+
+    class PlaceHolderFragment : Fragment() {
+
+        override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+        ): View? {
+            val rootView = inflater.inflate(R.layout.fragment_main, container, false)
+            rootView.section_label.text =
+                "Hello Dany from section ${arguments?.getInt(ARG_SECTION_NUMBER)}"
+
+            return rootView
+        }
+
+        companion object {
+            private val ARG_SECTION_NUMBER = "Section number"
+
+            fun newIntent(sectionNumber: Int): PlaceHolderFragment {
+                val fragment = PlaceHolderFragment()
+                val args = Bundle()
+
+                args.putInt(ARG_SECTION_NUMBER, sectionNumber)
+                fragment.arguments = args
+                return fragment
+
+            }
+        }
+    }
+}
