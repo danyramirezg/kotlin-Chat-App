6da520af88aba51e94027ffd56aba72cd99ffab3
danyramirezg <danyramirezg@gmail.com>
2021-04-12 11:21:06 -0500

    Change themes

---
+++ app/src/main/java/adapters/ChatsAdapter.kt
@@ -0,0 +1,37 @@
+package adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import listeners.ContactsClickListener
+import util.Contact
+
+//class ChatsAdapter(val chats: ArrayList<String>): RecyclerView.Adapter<ChatsAdapter.ChatsViewHolder>(){
+//
+//    override fun onCreateViewHolder(
+//        parent: ViewGroup,
+//        viewType: Int
+//    ): ChatsAdapter.ChatsViewHolder {
+//
+//      val view = LayoutInflater.from()
+//    }
+//
+//    override fun getItemCount(): Int {
+//        return chats.size
+//
+//    }
+//
+//    override fun onBindViewHolder(holder: ChatsAdapter.ChatsViewHolder, position: Int) {
+//        TODO("Not yet implemented")
+//    }
+//
+//    class ChatsViewHolder(view: View): RecyclerView.ViewHolder(view){
+//        fun bind(chats: ArrayList<String>, listener: ContactsClickListener){
+//
+//        }
+//
+//
+//    }
+//
+//}
