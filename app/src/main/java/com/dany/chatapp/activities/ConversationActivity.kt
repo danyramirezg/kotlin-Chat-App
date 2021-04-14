024adc2a1847453e5923caf6e6329d383a0bc17a
danyramirezg <danyramirezg@gmail.com>
2021-04-13 22:10:11 -0500

    Conversation Screen Layout

---
+++ app/src/main/java/com/dany/chatapp/activities/ConversationActivity.kt
@@ -0,0 +1,17 @@
+package com.dany.chatapp.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.dany.chatapp.R
+
+class ConversationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_conversation)
+    }
+
+    fun onSend(v: View){
+
+    }
+}
